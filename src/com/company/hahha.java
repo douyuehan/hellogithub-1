9e2d57a10d7f5740cc97bb89968544d1b0e9ddb0
jiyb <jobrong@outlook.com>
2018-12-05 14:20:14 +0800

    hello world add

---
+++ src/com/company/hahha.java
@@ -0,0 +1,10 @@
+package com.company;
+
+/**
+ * Created by Administrator on 2018/12/5.
+ */
+public class hahha {
+    public static void main(String[] args) {
+        System.out.println(123);
+    }
+}
